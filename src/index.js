68be35caea095ef501d4909c4cca5788a3449d2c
Danila <AngelofDest1ny@yandex.ru>
2023-10-20 16:58:17 +0300

    feat: complete solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2) return false;
+  const result = [];
+  str.split('').forEach(el => {
+    bracketsConfig.forEach(arr => {
+      const lastIndex = result.length - 1;
+      const [firstEl, secondEl] = arr;
+      if (firstEl === result[lastIndex] && secondEl === el) {
+        result.pop();
+      } else if (firstEl == el) {
+        result.push(el);
+      }
+    });
+  });
+  return !result.length;
+};
